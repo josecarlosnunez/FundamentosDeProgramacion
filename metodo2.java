1cbb42f1250d3531363325798a65177a0a61a0c0
josecarlosnunez <A01364718@itesm.mx>
2017-08-28 12:57:22 -0500

    El cuadrado de un numero

---
+++ metodo2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class metodo2
+    {
+      public static void main(String[] args)
+      {
+        System.out.printf("El cuadrado de %f es %f", 4.0, cuadrado(4.0));
+
+      }
+      public static double cuadrado (double numero)
+      {
+        double cuadrado;
+        cuadrado = numero*numero;
+        return cuadrado;
+      }
+
+    }
